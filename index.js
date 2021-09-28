51b5d00be74e624209c357cb41de3c963e8ca8e8
wsxedc54545 <50731647+wsxedc54545@users.noreply.github.com>
2021-09-28 11:53:52 +0800

    first commit
    no images folder

---
+++ index.js
@@ -0,0 +1,67 @@
+let videoBtn = document.querySelectorAll('.vid-btn')
+let searchBtn = document.querySelector('#search_btn')
+let searchBar = document.querySelector('.search-bar-container')
+
+let formBtn= document.querySelector('#user_btn')
+let loginForm = document.querySelector('.login-form-container')
+let formClose = document.querySelector('#form-close')
+
+let menu = document.querySelector('#menu-bar')
+let navbar = document.querySelector('.navbar')
+
+window.onscroll = () =>{
+  searchBtn.classList.remove('fa-times');
+  searchBar.classList.remove('hide');
+  menu.classList.remove('fa-times')
+  navbar.classList.remove('show')
+  loginForm.classList.remove('show')
+}
+
+formBtn.addEventListener('click',()=>{
+  loginForm.classList.add('show')
+})
+formClose.addEventListener('click',()=>{
+  loginForm.classList.remove('show')
+})
+
+menu.addEventListener('click',()=>{
+  menu.classList.toggle('fa-times')
+  navbar.classList.toggle('show')
+})
+
+videoBtn.forEach(btn => {
+  btn.addEventListener('click',()=>{
+    document.querySelector('.controls .active').classList.remove('active')
+    btn.classList.add('active')
+    let src = btn.getAttribute('data-src')
+    document.querySelector('#video-slider').src = src 
+  })
+})
+
+searchBtn.addEventListener('click',()=>{
+  searchBtn.classList.toggle('fa-times')
+  searchBar.classList.toggle('hide')
+})
+
+
+//review slider
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+  },
+});
+
